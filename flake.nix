2993f85da74ee58623a88fec985c60eff26310c6
Matt Provost <mattprovost6@gmail.com>
2024-06-19 12:22:28 -0500

    feat: add rust to workspace
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- flake.nix
+++ flake.nix
@@ -1,23 +1,27 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }: flake-utils.lib.eachDefaultSystem (system:
-    let
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShells = {
-        default = pkgs.mkShell {
-          buildInputs = [
-            pkgs.docker
-            pkgs.docker-compose
-          ];
+  outputs = { nixpkgs, rust-overlay, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        overlays = [ (import rust-overlay) ];
+        pkgs = import nixpkgs {
+          inherit system overlays;
         };
-      };
-    });
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            buildInputs = [
+              pkgs.docker
+              pkgs.docker-compose
+              pkgs.rust-bin.stable.latest.default
+            ];
+          };
+        };
+      });
 }
