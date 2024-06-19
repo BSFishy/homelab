9160c47d56dc9b25a135c669edce5196a426a6c8
Matt Provost <mattprovost6@gmail.com>
2024-06-19 12:33:45 -0500

    chore: establish baseline for bootstrap
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- bootstrap/src/main.rs
+++ bootstrap/src/main.rs
@@ -1,3 +1,9 @@
+use users::{get_current_gid, get_current_uid};
+
 fn main() {
-    println!("Hello, world!");
+    let gid = get_current_gid();
+    let uid = get_current_uid();
+
+    println!("Current gid: {}", gid);
+    println!("Current uid: {}", uid);
 }
