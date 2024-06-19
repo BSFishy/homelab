4e825fb3a0a0b34df5273c948ccd36e202f30c76
Matt Provost <mattprovost6@gmail.com>
2024-06-19 10:40:20 -0500

    chore: remove unnecessary shell hook
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- flake.nix
+++ flake.nix
@@ -17,34 +17,6 @@
             pkgs.docker
             pkgs.docker-compose
           ];
-
-          # shellHook = ''
-          #   DOCKERD_PID=$(pgrep dockerd)
-          #   if [ -z "$DOCKERD_PID" ]; then
-          #     echo "Starting Docker daemon..."
-          #     nohup sudo $(which dockerd) > /tmp/dockerd.log 2>&1 &
-          #     sleep 5
-          #     DOCKERD_PID=$(pgrep dockerd)
-          #     if [ -n "$DOCKERD_PID" ]; then
-          #       echo "Docker daemon started successfully with PID $DOCKERD_PID."
-          #     else
-          #       echo "Failed to start Docker daemon. Check /tmp/dockerd.log for details."
-          #     fi
-          #   else
-          #     echo "Docker daemon is already running with PID $DOCKERD_PID."
-          #   fi
-          # '';
-          #
-          # shellExit = ''
-          #   DOCKERD_PID=$(pgrep dockerd)
-          #   if [ -n "$DOCKERD_PID" ]; then
-          #     echo "Stopping Docker daemon with PID $DOCKERD_PID..."
-          #     sudo kill -SIGTERM $DOCKERD_PID
-          #     echo "Docker daemon stopped."
-          #   else
-          #     echo "No running Docker daemon found."
-          #   fi
-          # '';
         };
       };
     });
