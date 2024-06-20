efba501d2e037e59704affe7f23da5e01e15e630
Matt Provost <mattprovost6@gmail.com>
2024-06-19 22:59:00 -0500

    feat: generate docker-compose.yml with bootstrap
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- manager/src/bootstrap.rs
+++ manager/src/bootstrap.rs
@@ -1,5 +1,6 @@
 use ignore::Walk;
 use regex::{Captures, Regex, Replacer};
+use serde::{Deserialize, Serialize};
 use std::{
     fs::{read_to_string, write},
     path::{Path, PathBuf},
@@ -60,29 +61,55 @@ fn strip_in_extension(path: &Path) -> Option<PathBuf> {
     None
 }
 
+fn find_compose_files() -> Vec<String> {
+    glob::glob("**/compose.yml")
+        .expect("Failed to read glob pattern")
+        .map(|entry| entry.unwrap())
+        .map(|path| format!("{}", path.display()))
+        .collect()
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct DockerCompose {
+    name: String,
+    include: Vec<String>,
+}
+
 pub fn bootstrap() {
+    let current_dir = std::env::current_dir().unwrap();
+
     let re = Regex::new(r"\$\{([^}:]+)(?:\:([^}]+))?\}").unwrap();
 
-    for result in Walk::new(std::env::current_dir().unwrap()) {
+    for result in Walk::new(&current_dir) {
         let entry = result.unwrap();
         let path = entry.path();
         if path.is_dir() {
             continue;
         }
 
-        if !contains_in_dot(path) {
-            continue;
-        }
+        if contains_in_dot(path) {
+            let file_contents = read_to_string(path).unwrap();
+            let file_contents = re.replace_all(&file_contents, EnvReplacer);
 
-        let file_contents = read_to_string(path).unwrap();
-        let file_contents = re.replace_all(&file_contents, EnvReplacer);
+            if let Some(new_path) = strip_in_extension(path) {
+                write(&new_path, file_contents.as_ref()).unwrap();
 
-        if let Some(new_path) = strip_in_extension(path) {
-            write(&new_path, file_contents.as_ref()).unwrap();
-
-            println!("Wrote {}", new_path.display());
-        } else {
-            eprintln!("Failed to write {}", path.display());
+                println!("Wrote {}", new_path.display());
+            } else {
+                eprintln!("Failed to write {}", path.display());
+            }
         }
     }
+
+    let compose_files = find_compose_files();
+    let compose = DockerCompose {
+        name: "homelab".to_string(),
+        include: compose_files,
+    };
+
+    let file_contents = serde_yaml::to_string(&compose).unwrap();
+    let file_name = current_dir.join("docker-compose.yml");
+    write(&file_name, file_contents).unwrap();
+
+    println!("Wrote {}", file_name.display());
 }
