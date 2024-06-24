7bb753faff2e4156074838cd2aabb8778af6fa64
Matt Provost <mattprovost6@gmail.com>
2024-06-24 14:30:14 -0500

    chore: clean out manager
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,24 +1,3 @@
-use clap::Parser;
-
-mod args;
-mod bootstrap;
-mod cloudflare;
-mod config;
-mod configure;
-mod nginx;
-mod pihole;
-mod port_cache;
-mod service;
-mod template;
-mod token_cache;
-mod tunnel;
-
-fn main() {
-    use args::Command;
-
-    let args = args::Args::parse();
-
-    match args.command {
-        Command::Bootstrap(args) => bootstrap::bootstrap(args),
-    }
+pub fn main() {
+    println!("Hello world");
 }
