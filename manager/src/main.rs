53112be71db862c9a62fd58a226826bff4d6ecd1
Matt Provost <mattprovost6@gmail.com>
2024-06-24 14:51:30 -0500

    chore: begin scaffolding the application
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- manager/src/main.rs
+++ manager/src/main.rs
@@ -1,3 +1,11 @@
-pub fn main() {
+mod args;
+mod configure;
+mod deploy;
+mod external;
+mod logging;
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello world");
+
+    Ok(())
 }
