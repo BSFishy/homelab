97051ab6884755e870418e8b460877f1cb137f47
Matt Provost <mattprovost6@gmail.com>
2024-06-25 12:20:19 -0500

    chore: refactor log level negotiation
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

--- manager/src/logging.rs
+++ manager/src/logging.rs
@@ -3,34 +3,33 @@ use log::{Level, LevelFilter, Log};
 use std::env;
 
 pub fn setup(log_level: usize) -> Result<(), Box<dyn std::error::Error>> {
-    let log_level = match log_level {
-        1 => Ok(Level::Error),
-        2 => Ok(Level::Warn),
-        3 => Ok(Level::Info),
-        4 => Ok(Level::Debug),
-        5 => Ok(Level::Trace),
-        _ => Err("Invalid log level"),
-    }?;
+    let log_level = env::var("LOG_LEVEL")
+        .map(|level| level.to_lowercase())
+        .map_or(
+            match log_level {
+                1 => Ok(Level::Error),
+                2 => Ok(Level::Warn),
+                3 => Ok(Level::Info),
+                4 => Ok(Level::Debug),
+                5 => Ok(Level::Trace),
+                _ => Err("Invalid log level".to_string()),
+            },
+            |level| match level.as_str() {
+                "trace" => Ok(Level::Trace),
+                "debug" => Ok(Level::Debug),
+                "warn" | "warning" => Ok(Level::Warn),
+                "error" => Ok(Level::Error),
+                "info" => Ok(Level::Info),
+                _ => Err(format!("Unknown log level {level}")),
+            },
+        )?;
 
     let logger = Logger {
         log_level,
         term: Term::stderr(),
     };
 
-    let max_log_level = match env::var("LOG_LEVEL")
-        .unwrap_or("trace".to_string())
-        .to_lowercase()
-        .as_str()
-    {
-        "trace" => LevelFilter::Trace,
-        "debug" => LevelFilter::Debug,
-        "warn" | "warning" => LevelFilter::Warn,
-        "error" => LevelFilter::Error,
-        "off" => LevelFilter::Off,
-        "info" | _ => LevelFilter::Info,
-    };
-
-    log::set_boxed_logger(Box::new(logger)).map(|()| log::set_max_level(max_log_level))?;
+    log::set_boxed_logger(Box::new(logger)).map(|()| log::set_max_level(LevelFilter::Trace))?;
 
     Ok(())
 }
