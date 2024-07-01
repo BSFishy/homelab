beef7eec7a4effd28ce14195484a732c35ec5dcd
Matt Provost <mattprovost6@gmail.com>
2024-07-01 11:18:34 -0500

    feat: establish basic file structure
    Signed-off-by: Matt Provost <mattprovost6@gmail.com>

---
+++ services/system/index.ts
@@ -0,0 +1,1 @@
+export { output as traefik } from "./traefik";
---
+++ services/system/traefik.ts
@@ -0,0 +1,20 @@
+import * as k8s from "@pulumi/kubernetes";
+
+const traefik = new k8s.helm.v3.Chart("traefik", {
+  chart: "traefik",
+  fetchOpts: {
+    repo: "https://traefik.github.io/charts",
+  },
+  values: {
+    service: {
+      // Type must be ClusterIP instead of LoadBalancer on k3s
+      type: "ClusterIP",
+    },
+    ingressRoute: { dashboard: { entryPoints: ["web", "websecure"] } },
+  },
+});
+
+export let output = {
+  ip: traefik.getResourceProperty("v1/Service", "default", "traefik", "spec")
+    .clusterIP,
+};
